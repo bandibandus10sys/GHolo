6d844fe56abc018ee81a2c0d7deb77741de31c6b
bandibandus10 <bandifortnite@proton.me>
2025-03-09 20:23:31 +0100

    added on resource pack load refresh holograms

--- core/src/main/java/dev/geco/gholo/event/PlayerEventHandler.java
+++ core/src/main/java/dev/geco/gholo/event/PlayerEventHandler.java
@@ -1,20 +1,25 @@
 package dev.geco.gholo.event;
 
 import dev.geco.gholo.GHoloMain;
-import org.bukkit.entity.Player;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
 
 public class PlayerEventHandler implements Listener {
 
     private final GHoloMain gHoloMain;
+    private final Map<UUID, List<UUID>> loadingResourcePacks;
 
     public PlayerEventHandler(GHoloMain gHoloMain) {
         this.gHoloMain = gHoloMain;
+        this.loadingResourcePacks = new HashMap<>();        
     }
 
     @EventHandler
@@ -33,37 +38,39 @@ public class PlayerEventHandler implements Listener {
 
     @EventHandler
     public void playerQuitEvent(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-
-        gHoloMain.getHoloService().clearHolosCurrentContentForPlayer(player);
-
-        gHoloMain.getPacketHandler().removePlayerPacketHandler(player);
-        gHoloMain.getInteractionService().clearPlayerInteractions(player);
+        gHoloMain.getTaskService().runDelayed(() -> gHoloMain.getHoloService().loadHolosForPlayer(event.getPlayer()), 1);
     }
 
     @EventHandler
     public void playerChangedWorldEvent(PlayerChangedWorldEvent event) {
-        Player player = event.getPlayer();
-
-        gHoloMain.getHoloService().clearHolosCurrentContentForPlayer(player);
-
-        gHoloMain.getTaskService().runDelayed(() -> {
-            gHoloMain.getHoloService().loadHolosForPlayer(player);
-
-            gHoloMain.getInteractionService().loadInteractionsForPlayer(event.getPlayer());
-        }, false, player, 1);
+        gHoloMain.getTaskService().runDelayed(() -> gHoloMain.getHoloService().loadHolosForPlayer(event.getPlayer()), 1);
     }
 
-    @EventHandler
-    public void playerResourcePackStatusEvent(PlayerResourcePackStatusEvent event) {
-        if(event.getStatus() != PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED) return;
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onResourcePackStatus(PlayerResourcePackStatusEvent event) {
+        if (!event.getPlayer().isOnline()) return;
+        UUID playerUniqueId = event.getPlayer().getUniqueId();
+        UUID packUniqueId = getResourcePackID(event);
 
-        Player player = event.getPlayer();
-
-        gHoloMain.getTaskService().runDelayed(() -> {
-            gHoloMain.getHoloService().unloadHolosForPlayer(player);
-            gHoloMain.getHoloService().loadHolosForPlayer(player);
-        }, false, player, 1);
+        if (event.getStatus() == PlayerResourcePackStatusEvent.Status.ACCEPTED)
+            loadingResourcePacks.computeIfAbsent(playerUniqueId, (___) -> new ArrayList<>()).add(packUniqueId);
+        else if (event.getStatus() == PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED || event.getStatus() == PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD) {
+            loadingResourcePacks.computeIfAbsent(playerUniqueId, (___) -> new ArrayList<>()).removeIf(uuid -> uuid.equals(packUniqueId));
+            if (loadingResourcePacks.get(playerUniqueId) != null && loadingResourcePacks.get(playerUniqueId).isEmpty()) {
+                loadingResourcePacks.remove(playerUniqueId);
+                gHoloMain.getTaskService().runDelayed(() -> gHoloMain.getHoloService().loadHolosForPlayer(event.getPlayer()), 1);
+            }
+        }
     }
 
+    // For 1.20.2 and higher this method returns actual pack identifier, while for older versions, the identifier is a dummy UUID full of zeroes.
+    // Versions prior 1.20.2 supports sending and receiving only one resource-pack and a dummy, constant identifier can be used as a key.
+    private static @NotNull UUID getResourcePackID(PlayerResourcePackStatusEvent event) {
+        try {
+            event.getClass().getMethod("getID");
+            return event.getID();
+        } catch (final @NotNull NoSuchMethodException e) {
+            return new UUID(0,0);
+        }
+    }
 }
